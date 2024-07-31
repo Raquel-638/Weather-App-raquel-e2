4a5cdc560b2574fbbfc978d8d134f5e378203494
RaquelDiazDeLeon <173119840+Raquel-638@users.noreply.github.com>
2024-07-30 22:27:51 -0600

    entregable2-react-raquel

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import { useEffect } from 'react';
+import './App.css'
+import { useState } from 'react';
+import axios from 'axios';
+import WeatherCard from './components/WeatherCard';
+
+function App() {
+ 
+ const [coords, setCoords] = useState(null);
+ const [weather, setWeather] = useState(null);
+ const [temp, setTemp] = useState();
+ const [isLoading, setIsLoading] = useState(true);
+ const [hasError, setHasError] = useState(false);
+ const [showMessage, setShowMessage] = useState(false);
+ const [city, setCity] = useState('');
+ const [mesageError, setMesageError] = useState(false);
+ const [background, setBackground] = useState(false);
+
+ useEffect(() => {
+  setTimeout(() => {
+    setShowMessage(true);
+  }, 3000);
+  const success = (position) => {
+    setCoords({
+      lat: position.coords.latitude,
+      lon: position.coords.longitude
+    })
+  };
+
+  const error = () => {
+    setHasError(true)
+    setIsLoading(false)
+  }
+  navigator.geolocation.getCurrentPosition(success, error);
+ }, []);
+
+ useEffect(() => {
+  if (coords){
+
+    const APY_KEY = 'aef05036ac689a435c255943a19d1335';
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lat=${coords.lat}&lon=${coords.lon}&appid=${APY_KEY}`;
+
+    axios
+    .get(url)
+    .then((res) => {
+      setWeather(res.data)
+      const celsius = (res.data.main.temp - 273.15).toFixed(1);
+      const fahrenheit = ((celsius * 9)/ 5 + 32).toFixed(1);
+      setTemp({celsius, fahrenheit});
+      setMesageError(false);
+      setBackground(res.data.weather[0].description);
+    })
+    .catch((err) =>{ 
+      console.error(err);
+      setMesageError(true);
+    })
+    .finally(() => {
+      setIsLoading(false);
+    });
+  }
+ }, [coords, city]);
+
+ const objStyles ={
+    backgroundImage: `url('/img/${background}.jpg')`
+ }
+
+  return (
+    <div style={objStyles} className='app flex-container'>
+      {isLoading ? (
+        <div>
+          <div className="loader"></div>
+          {showMessage && <p>Per favore activa la localización</p>}
+        </div>
+      ) : hasError ? (
+        <h1>Si quieres ver la información del clima de tu ciudad, acepta los permisos y vuelve a cargar, per favore.</h1>
+      ) : (
+        <WeatherCard weather={weather} temp={temp} setCity={setCity} mesageError={mesageError} city={city}/>
+      )}
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,56 @@
+import { useRef, useState } from 'react';
+import './styles/WeatherCard.css';
+
+const WeatherCard = ({weather, temp, setCity, mesageError, city}) => {
+
+    const [isCelsius, setIsCelsius] = useState(true)
+    const changeDegrees = () => {
+        setIsCelsius(!isCelsius);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        setCity(inputSearch.current.value);   
+    };
+    
+    const inputSearch = useRef();
+
+  return <section className='card flex-container'>
+    <h1 className='card__title'>Weather App</h1>
+    <h2 className='card__country'>{weather?.name}, {weather?.sys.country}</h2>
+    <form onSubmit={handleSubmit}>
+        <input className='card__city' type="search" placeholder='Search by City' ref={inputSearch} />
+        <button>Search City</button>
+    </form>
+    {mesageError && <p>🤨Esa ciudad no existe...🤔</p>}
+    <article className='card__body grid-container'>
+        <div className='card__image-container'>
+            <img
+            className='card__image'
+            src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} alt={weather?.weather[0].main} />
+        </div>
+        <article className='info grid-conainer'>
+            <h3 className='info title'>{weather?.weather[0].description}
+            </h3>
+            <ul className="info__list grid-container">
+            <li className="info__item grid-container">
+              <span className="info__label">Wind Speed</span>
+              <span className="info__value">{weather?.wind.speed}m/s</span>
+            </li>
+            <li className="info__item grid-container">
+              <span className="info__label">Clouds</span>
+              <span className="info__value">{weather?.clouds.all}%</span>
+            </li>
+            <li className="info__item grid-container">
+              <span className="info__label">Pressure</span>
+              <span className="info__value">{weather?.main.pressure}hPa</span>
+            </li>
+          </ul>
+        </article>
+    </article>
+    <h2 className="card__temp">{isCelsius ? `${temp?.celsius}°C` : `${temp?.fahrenheit}°F`}</h2>
+    <button className="card__btn" onClick={changeDegrees}>Change to {isCelsius ? '°F' : '°C'}</button>
+  </section>
+};
+
+export default WeatherCard
